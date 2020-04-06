bfb6b84feb14c8df625f18c8f042b4eb3b4a7c75
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-04-06 14:21:15 +0200

    Made changes to contact form, added validated class

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,20 +1,25 @@
-import React from "react";
-import {Button, Col, Form, Row} from "react-bootstrap"
+import React, {useState} from "react";
+import {Button, Col, Form} from "react-bootstrap"
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import Heading from "../layout/Heading";
 import Description from "../layout/Description";
+import ErrorMessage from "../contact/ErrorMessage";
+
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const telephoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 const schema = yup.object().shape({
 
     firstName: yup
         .string()
-        .required("A first name is required"),
+        .required("Required"),
 
     lastName: yup
         .string()
-        .required("A last name is required"),
+        .required("Required"),
 
     telephoneNumber: yup
         .string().matches(telephoneRegExp, "The telephone number is not valid"),
@@ -23,53 +28,58 @@ const schema = yup.object().shape({
     email: yup
         .string()
         .email("Please enter a valid email")
-        .required("Email is required")
+        .required("Required")
 });
 
 
 function Contact() {
+    const [validated, setValidated] = useState(false);
     const { register, handleSubmit, errors } = useForm({
         validationSchema: schema
     });
 
     function onSubmit(data) {
         console.log("data", data);
+        setValidated(true);
     }
 
     return (
         <Col className="col-sm-10">
+            <div>
+    <FontAwesomeIcon icon={faHome} />
+  </div>
             <div className="formBox">
             <Heading title="Contact Us"/>
             <Description descriptionText="Et proident esse qui do officia est. Do amet aliquip nulla Lorem culpa laborum. Laboris proident mollit proident eiusmod amet irure nisi sunt. Irure irure in proident incididunt. Minim laboris cupidatat est amet irure. Ex elit id consequat eiusmod ullamco laborum eiusmod deserunt occaecat. Cupidatat laborum aute exercitation pariatur aliqua."/>
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <div className="labelNames">
-                    <Form.Label>Your Name:</Form.Label>
+                    
                     </div>
 
-                    <Form.Group className="formNames">
-                        
-                        
-                        <Form.Control name="firstName" placeholder="Firstname" ref={register} />
-                        {errors.firstName && <p>{errors.firstName.message}</p>}
-
-
-                        <Form.Control name="lastName" placeholder="Lastname" ref={register} />
-                        {errors.lastName && <p>{errors.lastName.message}</p>}
+                    <Form.Group>
+                        <Form.Label>First name:</Form.Label>
+                        <Form.Control name="firstName" placeholder="Your first Name" ref={register} />
+                        {errors.firstName && <ErrorMessage>{errors.firstName.message}</ErrorMessage>}
                     </Form.Group>
-
+                    <Form.Group>
+                        <Form.Label>Last name:</Form.Label>
+                        <Form.Control name="lastName" placeholder="Your last name" ref={register} />
+                        {errors.lastName && <ErrorMessage>{errors.lastName.message}</ErrorMessage>}
+                    </Form.Group>
                     <Form.Group>
                         <Form.Label>Telephone Number:</Form.Label>
                         <Form.Control name="telephoneNumber" placeholder="Example: 98765432" ref={register} />
-                        {errors.telephoneNumber && <p>{errors.telephoneNumber.message}</p>}
+                        {errors.telephoneNumber && <ErrorMessage>{errors.telephoneNumber.message}</ErrorMessage>}
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Email Address:</Form.Label>
-                        <Form.Control name="email" placeholder="E-mail" ref={register} />
-                        {errors.email && <p>{errors.email.message}</p>}
+                        <Form.Control name="email" placeholder="Your e-mail" ref={register} />
+                        {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+                        {validated && <div className="formValidated">Your request was validated!</div>}
                     </Form.Group>
-
                     <Button className="packageBtn" type="submit">Submit</Button>
+                    
                 </Form>
             </div>
         </Col>
